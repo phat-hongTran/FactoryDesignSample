fc17ba1af78b5597ac7c40af442d0b3a358a80ef
Phat Tran <ptran@strongtie.com>
2024-09-02 10:18:24 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FactoryDesignSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
